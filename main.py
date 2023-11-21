09e5aaa1f215836c7c8040b5a3113cf60c5a4fd9
Oleh Krupko <oleh.krupko@gmail.com>
2023-11-21 22:27:11 +0200

    test endpoint added

--- main.py
+++ main.py
@@ -91,3 +91,41 @@ def runner() -> dict:
 async def runner_async() -> dict:
     "Parse multiple feeds. Asynchronously."
     return await runner_async_func()
+
+
+@app.get("/test/", response_class=PrettyJSONResponse)
+async def test() -> dict:
+    "Parse multiple feeds."
+    t_start_total = time.perf_counter()
+
+    t_start = time.perf_counter()
+    result = runner()
+    t_end = time.perf_counter()
+    t_sync = t_end - t_start
+
+    t_start = time.perf_counter()
+    result_async = await runner_async()
+    t_end = time.perf_counter()
+    t_async = t_end - t_start
+
+    # how quicker async is
+    # measured in seconds
+    improvement = t_sync - t_async
+
+    t_end_total = time.perf_counter()
+    t_total = t_end_total - t_start_total
+
+    return {
+        'async_improvement': round(improvement, 10),
+        'total': round(t_total, 10),
+        'base': {
+            'result': result,
+            'result_len': len(result),
+            'time': round(t_sync, 2),
+        },
+        'base_async': {
+            'result': result_async,
+            'result_len': len(result_async),
+            'time': round(t_async, 2),
+        },
+    }
