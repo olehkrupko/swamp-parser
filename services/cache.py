e6424f9e27929319809fafb678b6992e9f4f6dfa
Oleh Krupko <oleh.krupko@gmail.com>
2024-05-03 02:39:16 +0300

    Cache: redis cache introduced

---
+++ services/cache.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime, timedelta
+
+import redis.asyncio as redis
+
+
+class Cache:
+    def url(self):
+        return f"swamp-parser:request:{self.href}"
+    
+    def timeout():
+        return datetime.now() + timedelta(hours=12)
+
+    async def get(self):
+        r = await redis.from_url(os.environ["REDIS"])
+        async with r.pipeline(transaction=True) as pipe:
+            return await pipe.get(self.cache_url())
+
+    async def set(self, value: str):
+        r = await redis.from_url(os.environ["REDIS"])
+        async with r.pipeline(transaction=True) as pipe:
+            await pipe.set(self.cache_url(), value)
+            pipe.expireat(self.cache_url(), self.cache_timeout())
