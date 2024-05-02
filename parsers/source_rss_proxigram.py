d482b3bb8f847f4f107af8e2eda417118c374fd4
Oleh Krupko <oleh.krupko@gmail.com>
2024-05-02 22:33:50 +0300

    Sources(ProxigramRssSource): reattempt failures

--- parsers/source_rss_proxigram.py
+++ parsers/source_rss_proxigram.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 from parsers.source_rss import RssSource
@@ -16,6 +17,32 @@ class ProxigramRssSource(RssSource):
             href += "/rss"
 
         return href
+
+    async def request(self, href):
+        global proxigram_semaphore
+        # avoiding connection overwhelming and status code 429
+        proxigram_semaphore = asyncio.Semaphore(1)
+
+        async with proxigram_semaphore:
+            return await super().request(href)
+
+    async def parse(self, response_str: str):
+        results = await super().parse(response_str=response_str)
+
+        attempt = 1
+        while not results and attempt < 10:
+            asyncio.sleep(3)
+            print(f"ProxigramRssSource.parse() {attempt=} {results=}")
+
+            # receive data
+            response_str = await self.request(href=self.href)
+
+            # process data
+            results = await super().parse(response_str=response_str)
+
+            attempt += 1
+
+        return results
 
     @staticmethod
     def each_name(each):
