8cb268369efa6ff0bbf362fec3f2340268854880
Oleh Krupko <oleh.krupko@gmail.com>
2024-06-23 05:41:11 +0300

    Source(ArtstationRss): refactoring

--- parsers/source_rss_artstation.py
+++ parsers/source_rss_artstation.py
@@ -1,13 +1,6 @@
 import logging
-import os
 
-import feedparser
-import random
 from parsers.source_rss import RssSource
-from sentry_sdk import capture_message
-
-from schemas.feed import ExplainedFeed
-from schemas.update import Update
 
 
 logger = logging.getLogger(__name__)
@@ -19,21 +12,9 @@ class ArtstationRssSource(RssSource):
         if "https://www.artstation.com/" in href:
             return True
 
+        # TODO: remove later
+        # it's left there to stop DDOSing unresponsive RSS
+        if "artstation.com/" in href:
+            return True
+
         return False
-
-    @staticmethod
-    def prepare_href(href: str) -> str:
-        username = href.replace("https://www.artstation.com/", "")
-        username = username.strip("/")
-
-        print(f"https://{username}.artstation.com/rss")
-        return f"https://{username}.artstation.com/rss"
-
-    async def explain(self) -> ExplainedFeed:
-        channel_id = self.href.split("?channel_id=")[-1]
-
-        feed = await super().explain()
-        feed["title"] += " - YouTube"
-        feed["href"] = f"https://www.youtube.com/channel/{channel_id}/videos"
-
-        return feed
