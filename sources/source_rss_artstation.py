73cf56c4d40fc905e707bd35032b0a300f1dca04
Oleh Krupko <oleh.krupko@gmail.com>
2024-07-11 20:27:30 +0300

    Source(ArtstationRss): draft simplified

--- sources/source_rss_artstation.py
+++ sources/source_rss_artstation.py
@@ -9,12 +9,7 @@ logger = logging.getLogger(__name__)
 class ArtstationRssSource(RssSource):
     @staticmethod
     def match(href: str):
-        if "https://www.artstation.com/" in href:
-            return True
-
-        # TODO: remove later
-        # it's left there to stop DDOSing unresponsive RSS
-        if "artstation.com/" in href:
+        if "artstation.com" in href:
             return True
 
         return False
