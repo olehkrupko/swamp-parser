195b181169461f253bdc46a4c0fa75f6d170fe88
Oleh Krupko <oleh.krupko@gmail.com>
2024-07-15 13:28:51 +0300

    Source(DeviantartRss): URL handling streamlined

--- sources/source_rss_deviantart.py
+++ sources/source_rss_deviantart.py
@@ -17,17 +17,23 @@ class DeviantartRssSource(RssSource):
 
     @staticmethod
     def prepare_href(href: str) -> str:
-        # 27 = len('https://www.deviantart.com/')
-        # 9 = len('/gallery/')
-        href = href[27:-9]
+        username = href.split("deviantart.com/")[-1]
+        username = username.split("/")[0]
         href_base = "https://backend.deviantart.com/rss.xml?type=deviation"
-        href = f"{href_base}&q=by%3A{ href }+sort%3Atime+meta%3Aall"
+        href = f"{ href_base }&q=by%3A{ username }+sort%3Atime+meta%3Aall"
 
         return href
 
     async def explain(self) -> ExplainedFeed:
         feed = await super().explain()
 
+        feed["title"] = feed["title"].replace("DeviantArt: ", "")
         feed["title"] += " - DeviantArt"
+
+        feed["href"] = self.href_original
+        if "/gallery" not in feed["href"]:
+            feed["href"] += "/gallery"
+
+        feed["private"] = True
 
         return feed
