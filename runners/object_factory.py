6aeb4f54bb5afb4fb038ae01c7fd901804cc179d
Oleh Krupko <oleh.krupko@gmail.com>
2024-11-11 16:41:04 +0200

    Core(ObjectFactory): TikTok disabled for now

--- runners/object_factory.py
+++ runners/object_factory.py
@@ -28,6 +28,7 @@ class ObjectFactory:
             return DisabledSource(href=href)
             return ProxigramRssSource(href=href)
         elif TiktokRssSource.match(href):
+            return DisabledSource(href=href)
             return TiktokRssSource(href=href)
         elif YoutubeRssSource.match(href):
             return YoutubeRssSource(href=href)
