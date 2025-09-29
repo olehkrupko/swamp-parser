1ea1a47bd84256df920a550e959101ff9202122c
Oleh Krupko <oleh.krupko@gmail.com>
2025-09-29 20:30:10 +0300

    Services(Bugsink): Disable unsupported features

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ sentry_sdk.init(
     # Add data like request headers and IP for users, if applicable;
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
-    # To collect profiles for all profile sessions,
-    # set `profile_session_sample_rate` to 1.0.
-    profile_session_sample_rate=1.0,
-    # Profiles will be automatically collected while
-    # there is an active span.
-    profile_lifecycle="trace",
-    # Enable logs to be sent to Sentry
-    enable_logs=True,
+    # # Set traces_sample_rate to 1.0 to capture 100%
+    # # of transactions for tracing.
+    # traces_sample_rate=1.0,
+    # # To collect profiles for all profile sessions,
+    # # set `profile_session_sample_rate` to 1.0.
+    # profile_session_sample_rate=1.0,
+    # # Profiles will be automatically collected while
+    # # there is an active span.
+    # profile_lifecycle="trace",
+    # # Enable logs to be sent to Sentry
+    # enable_logs=True,
 )
 
 
