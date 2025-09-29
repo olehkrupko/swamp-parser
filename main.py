b9b4efd3fd9ba8b0f37c67164515748e8113fa5f
Oleh Krupko <oleh.krupko@gmail.com>
2025-09-29 10:48:07 +0300

    Sentry: init Bugsink usage

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import asyncio
 from contextlib import asynccontextmanager
+from os import getenv
 
 from fastapi import FastAPI
+import sentry_sdk
 
 from routers import consumers, parsers, tests
 from workers.worker_parser_loop import ParserLoopWorker
 
 
-# from os import getenv
-# import sentry_sdk
-# sentry_sdk.init(
-#     dsn=getenv("SENTRY_SDK_DSN"),
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     traces_sample_rate=1.0,
-#     # Set profiles_sample_rate to 1.0 to profile 100%
-#     # of sampled transactions.
-#     # We recommend adjusting this value in production.
-#     profiles_sample_rate=1.0,
-# )
+sentry_sdk.init(
+    dsn=getenv("SENTRY_SDK_DSN"),
+    # Add data like request headers and IP for users, if applicable;
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # To collect profiles for all profile sessions,
+    # set `profile_session_sample_rate` to 1.0.
+    profile_session_sample_rate=1.0,
+    # Profiles will be automatically collected while
+    # there is an active span.
+    profile_lifecycle="trace",
+    # Enable logs to be sent to Sentry
+    enable_logs=True,
+)
 
 
 @asynccontextmanager
