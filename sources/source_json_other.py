df5288f612c728097c3460d83b3ad6d685cabf8c
Oleh Krupko <oleh.krupko@gmail.com>
2025-10-04 00:50:15 +0300

    Source(OtherJson): API updated

--- sources/source_json_other.py
+++ sources/source_json_other.py
@@ -13,6 +13,7 @@ class OtherJsonSource(JsonSource):
     async def request(self):
         return await super().request_via_random_proxy(
             href=self.href + "/posts",
+            headers={"Accept": "text/css"},
         )
 
     async def parse(self, response_str: str) -> list[Update]:
